561487c302c53c32ef4149771ef289f806a96eba
TedisAgolli <tedisagolli@gmail.com>
2019-10-26 17:18:19 -0400

    Take file path as arg

--- graph.py
+++ graph.py
@@ -1,12 +1,21 @@
-from graphviz import Graph
+import sys
+from graphviz import Digraph
 import yaml
 
-g = Graph('G',filename='compose.gv',engine='sfdp')
-with open('docker-compose.yml') as f:
-    compose = yaml.load(f,Loader=yaml.BaseLoader)
-    allNodes = list(compose['services'].keys())
-    for node in allNodes:
-        g.node(node)
-        for depends in compose['services'][node].get('depends_on',[]):
-            g.edge(node,depends)
-g.view()
+
+if __name__ == "__main__":
+    try:
+        fPath = sys.argv[1]
+    except IndexError:
+        print("Please path the compose file path")
+        exit()
+
+    g = Digraph("G", filename="compose.gv", engine="sfdp")
+    with open(fPath) as f:
+        compose = yaml.load(f, Loader=yaml.BaseLoader)
+        allNodes = list(compose["services"].keys())
+        for node in allNodes:
+            g.node(node)
+            for depends in compose["services"][node].get("depends_on", []):
+                g.edge(node, depends)
+    g.view()
