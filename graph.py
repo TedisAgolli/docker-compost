ef46e5a9fb096f78de29b920207e1d8a094876d5
TedisAgolli <tedisagolli@gmail.com>
2020-03-09 22:36:56 -0400

    Increase graph size and scale nodes by num edges in

--- graph.py
+++ graph.py
@@ -21,12 +21,19 @@ if __name__ == "__main__":
                 g.add_edge(node, depends)
 
     pos = nx.circular_layout(g)
+    plt.figure(figsize=(20, 14))
 
-    for node, coords in pos.items():
-        x, y = coords
-        shift = lambda val: 0.1 if val > 0 else -0.1
-        plt.text(x + shift(x), y + shift(y), node)
-
-    nx.draw_circular(g)
-    # plt.show()
+    nodeAttr = [max(len(list(g.in_edges(n))) * 1500, 1000) for n in list(nx.nodes(g))]
+    nx.draw_circular(
+        g,
+        node_size=nodeAttr,
+        node_color="lightblue",
+        linewidths=0.25,
+        font_size=10,
+        font_weight="bold",
+        with_labels=True,
+        dpi=1000,
+        scale=2,
+    )
+    plt.show()
     plt.savefig("compost.png")
