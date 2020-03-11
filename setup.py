491a8af932946ebd1426d8319f8098c5618223f5
TedisAgolli <tedisagolli@gmail.com>
2020-03-10 22:41:51 -0400

    Add setup file and put code in compost module

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="compost",
+    version="0.1",
+    packages=["compost"],
+    install_requires=["pyyaml", "networkx", "matplotlib"],
+    scripts=["compost/docker-compost"],
+)
