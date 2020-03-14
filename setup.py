88a88f61385f594dd94380b485bc83693794881d
TedisAgolli <tedisagolli@gmail.com>
2020-03-14 08:20:20 -0400

    Change name to docker-compost

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="compost",
+    name="docker-compost",
     version="0.1",
     packages=["compost"],
     install_requires=["pyyaml", "networkx", "matplotlib"],
+    setup_requires=["wheel"],
     scripts=["compost/compost"],
 )
